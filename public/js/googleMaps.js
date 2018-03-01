77a195eb87e461273689cfc209267c973ec54068
Cameron <cameronelee42@gmail.com>
2018-03-01 10:55:25 -0800

    lab8 stuff

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,19 @@
 function initMap() {
-	// add your code here
+	// Create center marker
+  // UCSD coords: 32.880, -117.236
+  // SSC Coords: 32.8788033,-117.2359121
+  var ltlng = {lat:32.8788033, lng:-117.2359121};
+
+  // Create a map object and specify the DOM element for display
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: ltlng,
+    zoom: 15
+  });
+
+  var marker = new google.maps.Marker({
+    position: ltlng,
+    map: map,
+    title: "SSC"
+  });
+
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,15 @@ var data = {
 exports.adminView = function(req, res){
   res.render('rsvp', data);
 };
+
+/*
+ * POST
+ */
+ exports.addRSVP = function(req, res){
+   console.log(rsvpEmail);
+
+   data.rsvp.push(rsvpEmail); // Add to current data
+
+   // Lets send our task name back so out browser knows it worked out!
+   res.send(rsvpEmail)
+ };
